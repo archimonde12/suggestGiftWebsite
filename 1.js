e9afb1449c4b15209fd42e19948b2e87b2a37bc1
HoHung <hung.homanh.14@gmail.com>
2020-04-09 20:04:12 +0700

    up code

---
+++ 1.js
@@ -0,0 +1,8 @@
+﻿ $(document).ready(function () {
+     $(".menu li").hover(function(){
+        $(this).find("ul:first").slideInDown(500)
+     },function()
+     {
+        $(this).find("ul:first").slideOutDown(500)
+     })
+ });
