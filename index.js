4a30e14384c45a215cf5e19fb76a892fc089db4a
HoHUng <hung.homanh.14@gmail.com>
2020-04-10 23:55:53 +0700

    up

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-﻿ $(document).ready(function () {
-     $(".menu li").hover(function(){
-        $(this).find("ul:first").slideInDown(500)
-     },function()
-     {
-        $(this).find("ul:first").slideOutDown(500)
-     })
- });
+﻿$(document).ready(function () {
+    $(".menu li").hover(function(){
+       $(this).find("ul:first").slideInDown(500)
+    },function()
+    {
+       $(this).find("ul:first").slideOutDown(500)
+    })
+});
